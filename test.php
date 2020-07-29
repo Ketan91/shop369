dd4dcc2715bed6c552080e4fe1f02df60bbcd3d8
Ketan91 <ketan@flickstree.com>
2020-07-29 18:34:33 +0530

    testing

--- test.php
+++ test.php
@@ -59,7 +59,6 @@ if ($result_banner = @file_get_contents("banner.json")) {
         <!--        <link async href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
                 <link async href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">-->
         <!-- Global site tag (gtag.js) - Google Analytics -->
-
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-55810795-26"></script>
 
         <script>
